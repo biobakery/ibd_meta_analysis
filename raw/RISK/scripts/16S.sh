3819cac2ac2e6ff1a0a2450bbdd02317baa55b3f
Siyuan Ma <sim653@mail.harvard.edu>
2018-05-21 16:01:49 -0400

    merge local with remote

--- raw/RISK/scripts/16S.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# generate sample identifiers and curate metadata
-
-hutlab load anadama2-0.4.0-devel
-hutlab load biobakery_workflows-0.9.0-devel
-
-biobakery_workflows 16s --input raw/RISK/16S/ --output processed/RISK/16S/
